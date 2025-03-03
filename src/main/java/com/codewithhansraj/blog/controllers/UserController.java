e42284e2bace9930c046b93a1659569da8049fdb
Hansraj Parmar <hansraj.ccs@gmail.com>
2025-03-03 08:22:11 +0530

    validation added

--- src/main/java/com/codewithhansraj/blog/controllers/UserController.java
+++ src/main/java/com/codewithhansraj/blog/controllers/UserController.java
@@ -1,8 +1,6 @@
 package com.codewithhansraj.blog.controllers;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codewithhansraj.blog.payloads.ApiResponse;
 import com.codewithhansraj.blog.payloads.UserDto;
 import com.codewithhansraj.blog.services.UserService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -30,7 +31,7 @@ public class UserController {
 	
 	//POST - Create User
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
 	}
@@ -38,7 +39,7 @@ public class UserController {
 	//PUT - Update user
 	
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
 		UserDto updatedUser = this.userService.updateUser(userDto, uid);		
 		return ResponseEntity.ok(updatedUser);
 	}
@@ -46,18 +47,18 @@ public class UserController {
 	//DELETE - Delete User
 	@DeleteMapping("/{userId}")
 	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid){
-		this.deleteUser(uid);
+		this.userService.deleteUser(uid);
 		return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted Successfully", true),  HttpStatus.OK);
 	}
 	
 	
-	//GET - user get
+	//GET - get all user
 	@GetMapping("/")
 	public ResponseEntity<List<UserDto>> getAllUsers() {
 		return ResponseEntity.ok(this.userService.getAllUsers());
 	}
 	
-	//GET - single user
+	//GET - get single user
 	@GetMapping("/{userId}")
 	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId) {
 		return ResponseEntity.ok(this.userService.getUserById(userId));
